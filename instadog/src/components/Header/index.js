bdbcf1a6264438d9e19733c239ceb20e8a36b620
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-07-03 17:20:06 -0700

    Implemented Header component & created classNames for various nav options

---
+++ instadog/src/components/Header/index.js
@@ -0,0 +1,22 @@
+// Imports
+// src/components/Header/index.js
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <nav className = 'Nav'>
+                <div className = 'Nav-menus'>
+                    <div className = 'Nav-brand'>
+                        <a className = 'Nav-brand-logo' href = '/'>
+                            InstaDog
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
+}
+
+// export
+export default Header;

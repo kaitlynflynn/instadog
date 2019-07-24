28485190491a217df0771de3c3bd6c747e3ecbf3
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-07-23 17:02:43 -0700

    Refactored code; added some notes

--- instadog/src/components/Post/index.js
+++ instadog/src/components/Post/index.js
@@ -1,40 +1,40 @@
 // src/components/Post/index.js
+
 // Imports
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Post.css';
 
 class Post extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        const nickname = this.props.nickname;
-        const avatar = this.props.avatar;
-        const image = this.props.image;
-        const caption = this.props.caption;
 
-        return <article className = 'Post' ref = 'Post'>
-            <header>
-                <div className = 'Post-user'>
-                    <div className = 'Post-user-avatar'>
-                        <img src = {avatar} alt = {nickname} />
-                    </div>
-                    <div className = 'Post-user-nickname'>
-                        <span>{nickname}</span>
-                    </div>
-                </div>
-            </header>
-            <div className = 'Post-image'>
-                <div className = 'Post-image-bg'>
-                    <img alt = {caption} src = {image} />
-                </div>
+    render() {
+      const nickname = this.props.nickname;
+      const avatar = this.props.avatar;
+      const image = this.props.image;
+      const caption = this.props.caption;
+      return (
+        <article className = 'Post' ref = 'Post'>
+          <header>
+            <div className = 'Post-user'>
+              <div className = 'Post-user-avatar'>
+                <img src = {avatar} alt = {nickname} />
+              </div>
+              <div className = 'Post-user-nickname'>
+                <span>{nickname}</span>
+              </div>
             </div>
-            <div className = 'Post-caption'>
-                <strong>{nickname}</strong>{caption}
+          </header>
+          <div className = 'Post-image'>
+            <div className = 'Post-image-bg'>
+              <img alt = {caption} src = {image} />
             </div>
-        </article>;
+          </div>
+          <div className = 'Post-caption'>
+            <strong>{nickname}</strong> {caption}
+          </div>
+        </article>
+      );
     }
-}
+  }
 
 // export
 export default Post;
